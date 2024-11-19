42f224b70cc6c2701c25cc04d138cbecbbb7b5bc
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-11-19 22:33:16 +0000

    Learned libraries and frameworks characteristics

--- index.js
+++ index.js
@@ -1,1 +1,91 @@
-ReactDOM.render(<h2>Test React</h2>, document.getElementById("root"))
+// First parameter of ReactDOM is what I want to place, and the second is where to place it 
+ReactDOM.render(<h1>Hello React</h1>,document.getElementById("root"));
+
+// We can wrtite multiple lines in the first parameter
+ReactDOM.render(<ul><li>Thing 1</li><li>Thing 2</li></ul>,
+document.getElementById("root"));
+
+// Navbar from bootstrap
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    )
+}
+
+// Challenge: Create your own custom React component!
+// Call it "MainContent", and have it return a simple
+// h1 element that says "I'm learning React!"
+
+// Afterward, render it below the Navbar (which
+// you can do inside the ReactDOM.render call below)
+
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent /> 
+    </div>,
+    document.getElementById("root")
+)
+
+// Declarative -> What should be done?: The program solve how to get it done -> React
+// Imperative -> How should it be done?: The program need every step of how to do it -> Vanilla JS
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+// I have to give step by step
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+history.className = "header";
+document.getElementById("root").append(h1);
+
+// React solve how to render it
+// ReactDOM.render(<h1 className="header">Hello, React!</h1>, document.getElementById("root"))
