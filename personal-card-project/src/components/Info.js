2ac5c2ef18dc9467db66393136843771ec092d62
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-11-28 22:54:56 +0000

    Started personal-card-project

---
+++ personal-card-project/src/components/Info.js
@@ -0,0 +1,14 @@
+export default function Info() {
+    return (
+        <div className="info--container">
+            <img src="headshot-1.png" className="info--headshot"></img>
+            <h1>Juan Martinez</h1>
+            <h3>Frontend Developer</h3>
+            <p>jcarlosmartinez.me</p>
+            <div className="info--buttons-box">
+                <button className="info--email-button">Email</button>
+                <button className="info--linkedin-button">LinkedIn</button>
+            </div>
+        </div>
+    )
+}
