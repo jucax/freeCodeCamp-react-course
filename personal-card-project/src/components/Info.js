451e1b81cf5c6c73298e43e2d1efc4ff787a7a06
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-11-29 22:53:43 +0000

    Finished Info component

--- personal-card-project/src/components/Info.js
+++ personal-card-project/src/components/Info.js
@@ -4,10 +4,16 @@ export default function Info() {
             <img src="headshot-1.png" className="info--headshot"></img>
             <h1>Juan Martinez</h1>
             <h3>Frontend Developer</h3>
-            <p>jcarlosmartinez.me</p>
+            <a href="jcarlosmartinez.me" ><p>jcarlosmartinez.me</p></a>
             <div className="info--buttons-box">
-                <button className="info--email-button">Email</button>
-                <button className="info--linkedin-button">LinkedIn</button>
+                <button className="info--email-button">
+                    <img src="mail-logo.png" className="info--email-logo"></img>
+                    Email
+                </button>
+                <button className="info--linkedin-button">
+                <img src="linkedin-logo.png" className="info--linkedin-logo"></img>
+                    LinkedIn
+                </button>
             </div>
         </div>
     )
