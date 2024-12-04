fc7017efb207eea4adf26a7a3463d8e01b3f85b5
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-12-04 19:15:00 +0000

    Learned conditional rendering

--- airbnb-experience-project/src/App.js
+++ airbnb-experience-project/src/App.js
@@ -10,12 +10,14 @@ import data from './data.js';
 function App() {
   const cards = data.map((card) => {
     return <Card 
+            key = {card.id}
             img = {card.coverImg}
-            rating = {card.stats.rating}
+            rating = {card.stats.rating} 
             reviewCount = {card.stats.reviewCount}
             location = {card.location}
             title = {card.title}
             price = {card.price}
+            openSpots = {card.openSpots}
            />
   })
 
--- airbnb-experience-project/src/components/Card.js
+++ airbnb-experience-project/src/components/Card.js
@@ -1,7 +1,15 @@
-export default function Card({img, rating, reviewCount, location, title, price}) {
+export default function Card({img, rating, reviewCount, location, title, price, openSpots}) {
     // If we need to combine a prop with other html, we can use {` ${}`} the back ticks as we do in vanila JS
+    // We can just use JS principles to make functions like conditional rendering
+    let badgeText;
+    if (openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (location === "Online") {
+        badgeText = "ONLINE";
+    }
     return (
         <div className="card">
+        {badgeText && <div className="card--badge">{badgeText}</div>}  
             <img src = {img} className = "card--image"/>
             <div className="card--stats">
                 <img src="star.png" className="card--star"/>
